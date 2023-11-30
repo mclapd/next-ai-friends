dd28dc8ae392d22d6bf3fb2afe32a5bd19b236d0
one_punch_man <ckim@banlaw.com>
2023-11-30 18:56:00 +1100

    Sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Menu } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import { Sparkles } from "lucide-react";
 import { Button } from "./ui/button";
-import { ModeToggle } from "./mode-toggle";
+import { ModeToggle } from "@/components/mode-toggle";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const font = Poppins({
   weight: "600",
@@ -16,9 +16,9 @@ const font = Poppins({
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
           <h1
             className={cn(
